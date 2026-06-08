d956ecec8f18438cf9b16388640158b33e6fbece
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2026-06-08 12:56:54 -0300

    new xpp toolbar

--- home-manager/xournalpp.nix
+++ home-manager/xournalpp.nix
@@ -4,15 +4,8 @@
   home.packages = [ pkgs.xournalpp ];
 
   xdg.configFile."xournalpp/toolbar.ini".text = ''
-    [Saved Toolbars]
-    version=1
-
     [Default]
-    Name=Minimal Single Toolbar
-    ToolbarsTop1=PEN,SEPARATOR,TOOL_RULER,TOOL_RECOGNIZER,SEPARATOR,SELECT_REGION,SELECT_RECTANGLE,SEPARATOR,COLOR(0x000000),COLOR(0xff0000),COLOR(0x0000ff),COLOR(0x00cc00)
-    ToolbarsTop2=
-    ToolbarsBottom=
-    ToolbarsLeft=
-    ToolbarsRight=
+    toolbarTop1=PEN,PLAIN,DOTTED,SELECT_RECTANGLE,COMPASS,SETSQUARE,SEPARATOR,TEXT,MATH_TEX,DRAW_ARROW,RULER,DRAW_COORDINATE_SYSTEM,DRAW_RECTANGLE,DRAW_ELLIPSE,TOOL_FILL,SEPARATOR,COLOR(10),COLOR(0),COLOR(6),COLOR(3),COLOR(2),COLOR(7)
+    name=custom
   '';
 }
