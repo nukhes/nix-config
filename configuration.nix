d1efcd58b94e3b1b342960432bfd913ec430c08e
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2026-06-09 21:32:57 -0300

    trim ssd

--- configuration.nix
+++ configuration.nix
@@ -21,6 +21,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages;
+  services.fstrim.enable = true;
 
   users.users."user" = {
     isNormalUser = true;
