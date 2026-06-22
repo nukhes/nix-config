baed1342039e8f7d9aa91df23b4467a97005195a
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2026-06-22 12:33:29 -0300

    new approach for wacom issue

--- nixosModules/services/wacom.nix
+++ nixosModules/services/wacom.nix
@@ -1,38 +1,21 @@
 { config, pkgs, ... }:
-let
-  mapWacomScript = pkgs.writeShellScriptBin "map-wacom" ''
-    sleep 2
 
-    X_USER=$(${pkgs.coreutils}/bin/who | ${pkgs.gnugrep}/bin/grep '(:0)' | ${pkgs.gawk}/bin/awk '{print $1}' | ${pkgs.coreutils}/bin/head -n 1)
-    if [ -z "$X_USER" ]; then
-      X_USER=$(${pkgs.coreutils}/bin/ls /home | ${pkgs.coreutils}/bin/head -n 1)
-    fi
-
-    USER_ID=$(${pkgs.coreutils}/bin/id -u "$X_USER")
-
-    export DISPLAY=:0
-    if [ -f "/home/$X_USER/.Xauthority" ]; then
-      export XAUTHORITY=/home/$X_USER/.Xauthority
-    else
-      export XAUTHORITY=/run/user/$USER_ID/gdm/Xauthority
-    fi
-
-    ${pkgs.xinput}/bin/xinput map-to-output "Wacom One by Wacom S Pen stylus" eDP-1
-  '';
-in
 {
+  imports = [
+    ./usb-hook.nix
+  ];
+
   services.xserver.wacom.enable = true;
 
-  services.udev.extraRules = ''
-    ACTION=="add", SUBSYSTEM=="input", ATTRS{name}=="Wacom One by Wacom S Pen Pen (0)", TAG+="systemd", ENV{SYSTEMD_WANTS}+="map-wacom.service"
-  '';
-
-  systemd.services.map-wacom = {
-    description = "Map Wacom tablet to eDP-1 display";
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = "${mapWacomScript}/bin/map-wacom";
-      User = "root";
-    };
+  services.usb-hook = {
+    enable = true;
+    script = '' 
+      export DISPLAY=:0
+      MY_USER="user"
+      USER_ID=$(${pkgs.coreutils}/bin/id -u $MY_USER)
+      export XAUTHORITY="/run/user/$USER_ID/.lyxauth"
+      ${pkgs.coreutils}/bin/sleep 3
+      ${pkgs.sudo}/bin/sudo -u $MY_USER env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY ${pkgs.xinput}/bin/xinput map-to-output "Wacom One by Wacom S Pen stylus" eDP-1
+    '';
   };
 }
