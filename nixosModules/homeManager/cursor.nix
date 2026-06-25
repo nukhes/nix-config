9b94121276f1f18bbd5f9fe247414e9883c02b47
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2026-06-25 09:39:29 -0300

    new cursor

---
+++ nixosModules/homeManager/cursor.nix
@@ -0,0 +1,11 @@
+{ config, pkgs, ... }:
+
+{
+  home.pointerCursor = {
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Ice";
+    size = 24;
+    x11.enable = true;
+    gtk.enable = true;
+  };
+}
