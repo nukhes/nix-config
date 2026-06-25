5088f969b11ca9cf78565dbc0ca12a37dea771cb
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2026-06-25 13:20:22 -0300

    add steam

---
+++ nixosModules/programs/steam.nix
@@ -0,0 +1,12 @@
+{ pkgs, ... }: {
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; 
+    dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+  };
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+}
