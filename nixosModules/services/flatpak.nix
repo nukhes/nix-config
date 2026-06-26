657c5e18174424356147405f8d763dfe6163b6b0
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2026-06-26 15:11:44 -0300

    ensure bottles flatpak can access my games

--- nixosModules/services/flatpak.nix
+++ nixosModules/services/flatpak.nix
@@ -7,4 +7,15 @@
     xdg.portal.enable = true;
     xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
     services.flatpak.enable = true;
+    services.flatpak.overrides = {
+        "com.usebottles.bottles" = {
+            filesystems = [
+                "~/.local/share/games"
+                "xdg-run/dri:ro" 
+            ];
+            environment = {
+                "XDG_DATA_DIRS" = "/run/current-system/sw/share:/share";
+            };
+        };
+    };
 }
