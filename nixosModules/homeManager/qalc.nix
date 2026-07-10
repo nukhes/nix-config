6cdf72c8a4f745885f9c7b93bd867cd1531d327a
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2026-07-10 16:06:34 -0300

    calc

---
+++ nixosModules/homeManager/qalc.nix
@@ -0,0 +1,22 @@
+{ pkgs, ... }:
+
+{
+  home.packages = with pkgs; [
+    qalc
+    qalculate-qt
+  ];
+
+  home.file.".config/qalculate/qalculate.cfg" = {
+    text = ''
+      [General]
+      colorize=1
+      decimals=6
+      decimal_comma=1
+      fraction_mode=1
+      auto_completion=1
+      save_history=1
+    '';
+    force = true; 
+  };
+}
+
