7ac8f7defc4c84ddc9077eabce6880ac82c51e4d
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2026-07-12 15:03:29 -0300

    fuck prochot

--- hosts/hackbook/configuration.nix
+++ hosts/hackbook/configuration.nix
@@ -27,6 +27,7 @@
     "acpi_osi=Darwin"
     "intel_pstate=disable"
     "pcie_aspm=force"
+    "lsm=capability,yama"
     "mem_sleep_default=s2idle"
   ];
 
@@ -40,18 +41,6 @@
   networking.networkmanager.wifi.powersave = false;
   systemd.targets.hibernate.enable = false;
   systemd.targets.hybrid-sleep.enable = false;
-
-  services.thermald.enable = true;
-  services.throttled = {
-    enable = true;
-    extraConfig = ''
-      [AC]
-      BDPROCHOT = False
-
-      [BATTERY]
-      BDPROCHOT = False
-    '';
-  };
 
   nixpkgs.config.allowInsecurePredicate = pkg: (builtins.elem (lib.getName pkg) [ "broadcom-sta" ]);
 
