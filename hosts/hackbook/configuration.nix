a8c86ab844647d80ba2ca468a0ead9924df8caee
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2026-07-12 08:55:33 -0300

    fix bd prochot plsssssssss

--- hosts/hackbook/configuration.nix
+++ hosts/hackbook/configuration.nix
@@ -25,7 +25,7 @@
     "i915.enable_fbc=1"
     "i915.enable_guc=3"
     "acpi_osi=Darwin"
-    "acpi_mask_gpe=0x17"
+    "intel_pstate=disable"
     "pcie_aspm=force"
     "mem_sleep_default=s2idle"
   ];
@@ -40,6 +40,18 @@
   networking.networkmanager.wifi.powersave = false;
   systemd.targets.hibernate.enable = false;
   systemd.targets.hybrid-sleep.enable = false;
+
+  services.thermald.enable = true;
+  services.throttled = {
+    enable = true;
+    extraConfig = ''
+      [AC]
+      BDPROCHOT = False
+
+      [BATTERY]
+      BDPROCHOT = False
+    '';
+  };
 
   nixpkgs.config.allowInsecurePredicate = pkg: (builtins.elem (lib.getName pkg) [ "broadcom-sta" ]);
 
