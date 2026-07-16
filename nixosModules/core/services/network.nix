96330c22b5471c287ca299ad9a2642981ded275d
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2026-07-16 16:20:15 -0300

    remove eduroam.nix (keep everything in network.nix)

--- nixosModules/core/services/network.nix
+++ nixosModules/core/services/network.nix
@@ -6,4 +6,17 @@
 {
   networking.networkmanager.enable = true;
   networking.wireless.enable = true;
+
+  age.identityPaths = [
+    "/etc/ssh/ssh_host_ed25519_key"
+  ];
+
+  age.secrets.eduroam = {
+    file = ../../../secrets/eduroam.age;
+    path = "/etc/NetworkManager/system-connections/eduroam.nmconnection";
+    mode = "0600";
+    owner = "root";
+    group = "root";
+    symlink = false;
+  };
 }
