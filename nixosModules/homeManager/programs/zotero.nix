5bac13a190189df40ad4d9b6b5d7e3fc2c30017d
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2026-07-18 20:38:26 -0300

    fix

--- nixosModules/homeManager/programs/zotero.nix
+++ nixosModules/homeManager/programs/zotero.nix
@@ -5,30 +5,33 @@
     pkgs.zotero
   ];
 
-  home.file.".zotero/zotero/default_profile/prefs.js".text = ''
-    user_pref("accessibility.typeaheadfind", false); // Desativa busca ao digitar
-    user_pref("browser.tabs.remote.autostart", false); // Economiza RAM em PCs antigos
-    user_pref("javascript.options.mem.gc_per_zone", true); // Gerenciamento de memória agressivo
-    user_pref("extensions.zotero.attachments.textExtraction.characters", 0); // Desativa extração de texto de PDFs
-    user_pref("extensions.zotero.attachments.textExtraction.pages", 0); // Evita uso de CPU em segundo plano
-    user_pref("extensions.zotero.indexing.maxPages", 0); // Zera páginas indexadas
-    user_pref("extensions.zotero.indexing.maxCharacters", 0); // Desativa indexação de busca interna
-    user_pref("datareporting.healthreport.uploadEnabled", false); // Desativa envio de dados
-    user_pref("toolkit.telemetry.enabled", false); // Desativa telemetria Mozilla/Zotero
-    user_pref("extensions.zotero.parsePDFs", false); // Não tenta ler metadados de PDF na internet automaticamente
-    user_pref("extensions.zotero.sync.auto", false); // Sincroniza apenas quando você clicar (evita travamentos)
-    user_pref("extensions.zotero.sync.storage.enabled", false); // Desativa download automático de arquivos pesados
-    user_pref("extensions.zotero.automaticTags", false); // Não cria tags automáticas baseadas em artigos
+  home.file.".zotero/zotero/default_profile/user.js".text = ''
+    user_pref("accessibility.typeaheadfind", false);
+    user_pref("browser.tabs.remote.autostart", false);
+    user_pref("javascript.options.mem.gc_per_zone", true);
+    user_pref("extensions.zotero.attachments.textExtraction.characters", 0);
+    user_pref("extensions.zotero.attachments.textExtraction.pages", 0);
+    user_pref("extensions.zotero.indexing.maxPages", 0);
+    user_pref("extensions.zotero.indexing.maxCharacters", 0);
+    user_pref("datareporting.healthreport.uploadEnabled", false);
+    user_pref("toolkit.telemetry.enabled", false);
+    user_pref("extensions.zotero.parsePDFs", false);
+    user_pref("extensions.zotero.sync.auto", false);
+    user_pref("extensions.zotero.sync.storage.enabled", false);
+    user_pref("extensions.zotero.automaticTags", false);
+    user_pref("extensions.zotero.useDataDir", true);
+    user_pref("extensions.zotero.dataDir", "/home/user/.local/share/zotero");
   '';
 
-  xdg.configFile."Zotero/Zotero/profiles.ini".text = ''
+  home.file.".zotero/zotero/profiles.ini".text = ''
     [General]
     StartWithLastProfile=1
+    Version=2
 
     [Profile0]
-    Name=default
+    Name=default_profile
     IsRelative=1
-    Path=../../.zotero/zotero/default_profile
+    Path=default_profile
     Default=1
   '';
 }
