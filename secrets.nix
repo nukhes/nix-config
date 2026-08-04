65d095b40a362fc71c844dde57a4ece8cb875537
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2026-08-04 09:11:53 -0300

    Add secrets/zai-api-key.age

--- secrets.nix
+++ secrets.nix
@@ -13,4 +13,6 @@ in
   "secrets/gemini-p052.age".publicKeys = allKeys;
   "secrets/openrouter-p052.age".publicKeys = allKeys;
   "secrets/spotify-player.age".publicKeys = allKeys;
+  "secrets/spotify-player.age".publicKeys = allKeys;
+  "secrets/zai-api-key.age".publicKeys = allKeys;
 }
