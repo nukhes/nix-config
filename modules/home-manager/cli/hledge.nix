149010bf667732538b05bfd97f54ad8fca4c6aaa
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2026-08-06 15:35:05 -0300

    simplificando o hledger

--- modules/home-manager/cli/hledge.nix
+++ modules/home-manager/cli/hledge.nix
@@ -10,15 +10,7 @@
     hl   = "hledger";
     hlui = "hledger-ui";
 
-    hl-balanco  = "hledger bal --forecast";
-    hl-print  = "hledger print --forecast";
-    hl-registro = "hledger register --forecast";
-
+    hl-balanco  = "hledger bal assets";
     hl-gastos = "hledger bal expenses --forecast";
-    hl-ra     = "hledger bal assets:unicamp:RA --forecast";
-
-    hl-hoje = "hledger register --forecast date:today";
-    hl-mes  = "hledger bal --forecast date:thismonth";
-    hl-prox = "hledger register --forecast date:today-2026/12/31";
   };
 }
