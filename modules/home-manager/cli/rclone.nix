64544145e066a5d3406b5f1c3c35b487e9f5badc
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2026-08-07 13:19:10 -0300

    remove zotero from rclone

--- modules/home-manager/cli/rclone.nix
+++ modules/home-manager/cli/rclone.nix
@@ -21,11 +21,6 @@ let
       remote = "p322814:library";
       local = "${config.home.homeDirectory}/.local/share/calibre-library";
     }
-    {
-      name = "zotero";
-      remote = "p322814:zotero";
-      local = "${config.home.homeDirectory}/.local/share/zotero";
-    }
   ];
 
   rclone = "${pkgs.rclone}/bin/rclone";
