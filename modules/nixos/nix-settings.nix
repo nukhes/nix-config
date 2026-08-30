258b81f6b322a6c3f1cf484264f4728cb731fc34
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2026-08-30 09:22:55 -0300

    refactor: update nix settings by removing documentation options and setting max-jobs to 1

--- modules/nixos/nix-settings.nix
+++ modules/nixos/nix-settings.nix
@@ -5,14 +5,23 @@
     overlays = [ inputs.nur.overlays.default ];
   };
 
-  documentation.enable = false;
-  documentation.nixos.enable = false;
-  documentation.man.enable = false;
-
   nix.settings = {
-    max-jobs = "auto";
-    allow-import-from-derivation = false;
+    substituters = [
+      "https://cache.nixos.org/"
+      "https://nix-community.cachix.org"
+    ];
+
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+    
     fallback = false;
+    allow-import-from-derivation = false;
+
+    max-jobs = 1;
+    cores = 1;
+
     experimental-features = [
       "nix-command"
       "flakes"
