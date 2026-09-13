899f4650e332f31b5b5f509ad559eea48538066a
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2026-09-13 10:21:01 -0300

    bs

--- modules/home-manager/programs/balena-etcher.nix
+++ modules/home-manager/programs/balena-etcher.nix
@@ -1,66 +1,70 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs, ... }:
 
-pkgs.stdenv.mkDerivation rec {
-  pname = "balena-etcher";
-  version = "2.1.6";
+let
+  balena-etcher = pkgs.stdenv.mkDerivation rec {
+    pname = "balena-etcher";
+    version = "2.1.6";
 
-  src = pkgs.fetchurl {
-    url = "https://github.com{version}/balenaEtcher-linux-x64-${version}.zip";
-    sha256 = "sha256-4YtYwRzI7Y40GAs9XNqbyHw+6G7rN9e8v5Xw4Xw4Xw4="; 
+    src = pkgs.fetchurl {
+      url = "https://github.com${version}/balenaEtcher-linux-x64-${version}.zip";
+      sha256 = "sha256-4YtYwRzI7Y40GAs9XNqbyHw+6G7rN9e8v5Xw4Xw4Xw4=";
+    };
+
+    nativeBuildInputs = with pkgs; [
+      unzip
+      autoPatchelfHook
+      makeWrapper
+    ];
+
+    buildInputs = with pkgs; [
+      stdenv.cc.cc.lib
+      nspr
+      nss
+      glib
+      gtk3
+      atk
+      cairo
+      pango
+      alsa-lib
+      libdrm
+      mesa
+      cups
+      systemd
+      libuuid
+      at-spi2-core
+      libxshmfence
+      xorg.libX11
+      xorg.libXcomposite
+      xorg.libXdamage
+      xorg.libXext
+      xorg.libXfixes
+      xorg.libXrandr
+      xorg.libXrender
+      xorg.libxcb
+      xorg.libXScrnSaver
+      xorg.libXtst
+    ];
+
+    unpackPhase = ''
+      unzip $src
+    '';
+
+    installPhase = ''
+      mkdir -p $out/bin $out/opt/balena-etcher
+
+      # Copia todo o conteúdo extraído para a pasta opt
+      cp -r balenaEtcher-linux-x64/* $out/opt/balena-etcher/
+
+      # Cria o link simbólico corrigido na pasta bin do sistema
+      ln -s $out/opt/balena-etcher/balena-etcher $out/bin/balena-etcher
+    '';
+
+    postFixup = ''
+      wrapProgram $out/bin/balena-etcher \
+        --add-flags "--no-sandbox"
+    '';
   };
-
-  nativeBuildInputs = with pkgs; [
-    unzip
-    autoPatchelfHook
-    makeWrapper
-  ];
-
-  buildInputs = with pkgs; [
-    stdenv.cc.cc.lib
-    nspr
-    nss
-    glib
-    gtk3
-    atk
-    cairo
-    pango
-    alsa-lib
-    libdrm
-    mesa
-    cups
-    systemd
-    libuuid
-    at-spi2-core
-    libxshmfence
-    xorg.libX11
-    xorg.libXcomposite
-    xorg.libXdamage
-    xorg.libXext
-    xorg.libXfixes
-    xorg.libXrandr
-    xorg.libXrender
-    xorg.libXxcb
-    xorg.libXScrnSaver
-    xorg.libXtst
-  ];
-
-  unpackPhase = ''
-    unzip $src
-  '';
-
-  installPhase = ''
-    mkdir -p $out/bin $out/opt/balena-etcher
-    
-    # Copia todo o conteúdo extraído para a pasta opt
-    cp -r balenaEtcher-linux-x64/* $out/opt/balena-etcher/
-
-    # Cria o link simbólico corrigido na pasta bin do sistema
-    ln -s $out/opt/balena-etcher/balena-etcher $out/bin/balena-etcher
-  '';
-
-  postFixup = ''
-    wrapProgram $out/bin/balena-etcher \
-      --add-flags "--no-sandbox"
-  '';
+in
+{
+  home.packages = [ balena-etcher ];
 }
-
